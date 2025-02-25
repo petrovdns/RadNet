0390710f8f31ce2b7bea4359e148d88b2d314688
petrovdns <petrovdns1@gmail.com>
2025-02-25 12:32:07 +0200

    #1: Initialized Radnet Project with Spring Boot Setup
    - Set up the initial Spring Boot project structure for Radnet.
    - Configured application properties for basic database connection and Spring Boot settings.

---
+++ src/main/java/com/petrovdns/radnet/RadNetApplication.java
@@ -0,0 +1,13 @@
+package com.petrovdns.radnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petrovdns/radnet/RadNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrovdns.radnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
